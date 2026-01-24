bf0af1dbbcbaad7ea1108f0ad52969f545dc7030
Andrew Pham <phama321@outlook.com>
2026-01-24 02:21:21 -0600

    Scroll Animation

---
+++ script.js
@@ -0,0 +1,24 @@
+//Scroll Animation
+document.addEventListener("DOMContentLoaded", function() {
+
+    //Get all resume section
+    const section = document.querySelectorAll('.resume-section, .work-item, .education-item, .involvement-item, .project-item')
+
+    //Detects when an element appears on the screen
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+
+            if(entry.isIntersecting){
+                entry.target.classList.add('animate-in');
+            }
+        });
+    }, {
+        threshold: 0.1,  //Triggers when the element is 10% visible
+        //
+        rootMargin: '0px 0px -50px 0px' //Starts the animation 50px before the element appears
+    });
+    //Observes all sections
+    section.forEach(section => {
+        observer.observe(section);
+    });
+});
